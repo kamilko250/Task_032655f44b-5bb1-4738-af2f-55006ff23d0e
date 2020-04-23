d5913f042f4eba83f8b4224a7ee175557bcf1f88
kamilko250 <freshmultikk21@gmail.com>
2020-04-23 20:40:28 +0200

    Działające
    Porównanie działa dla wywołania : 'python check_numbers.py numbers.txt good_answer' oraz 'python check_numbers.py numbers.txt'

--- check_numbers.py
+++ check_numbers.py
@@ -1,26 +1,46 @@
 #Napisz program
-import sys
+import sys,qrtools
 from sympy import isprime
+doCompare = False
 if(len(sys.argv) > 1):
      if(sys.argv[1] != 0):
           filename = sys.argv[1]
+          if(len(sys.argv) == 3):
+               if(sys.argv[2] != 0):
+                    qr = qrtools.QR()
+                    qr.decode(sys.argv[2])
+                    mydata = qr.data
+                    x = mydata.encode('utf-8')
+                    x = x.split()
+                    doCompare = True   
 else:
      filename = 0
      print("No file to check. Use 'python check_numbers.py filetocheckname.txt'")
+     
 if(filename != 0):     
      file = open(filename, "r")
      line = file.readline()
      result = "["
-     isFirst = True
+     isFirstElement = True
      while( line != ''):
+          i = 0
           splitedline = line.split()  
           splitedline[1] = int(splitedline[1],16)
           line = file.readline()
           if(isprime(splitedline[1])):
-               if(isFirst):
+               if(isFirstElement):
                     result = result + splitedline[0]
-                    isFirst = False
+                    i = i + 1
+                    isFirstElement = False
+                    
                else:    
-                    result = result + ", " + splitedline[0]  
+                    result = result + ", " + splitedline[0]
+               if(doCompare == True):
+                         for word in x:
+                              if(splitedline[0] == word):
+                                   print(splitedline[0])
+                                   print("good result")
+
+                    
      file.close()
      print (result + "]")
